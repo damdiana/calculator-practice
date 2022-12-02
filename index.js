b45cc071796ae74af0a96be19b4ce457f1d6439d
Diana Damian <diana.damiaan@gmail.com>
2022-12-02 18:11:29 +0200

    Implement support for operations (#3)

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ let btn7 = document.querySelector('#btn-7');
 let btn8 = document.querySelector('#btn-8');
 let btn9 = document.querySelector('#btn-9');
 let btnClear = document.querySelector('#clearBtn');
+let btnSubtraction = document.querySelector('#btn-subtraction');
+let btnAddition = document.querySelector('#btn-addition');
+let btnDivision = document.querySelector('#btn-division');
+let btnMultiplication = document.querySelector('#btn-multiplication');
 
+let operation;
 
 let nr1 = 0;
 
@@ -69,3 +74,23 @@ btnClear.addEventListener('click', function () {
     console.log(nr1);
 
 });
+
+btnSubtraction.addEventListener('click', function () {
+    operation = '-';
+    console.log(operation);
+});
+
+btnAddition.addEventListener('click', function () {
+    operation = '+';
+    console.log(operation);
+})
+
+btnDivision.addEventListener('click', function () {
+    operation = "/";
+    console.log(operation);
+})
+
+btnMultiplication.addEventListener('click', function () {
+    operation = '*';
+    console.log(operation);
+})
