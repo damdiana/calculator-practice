85c9052443545d8a06050100a0a51fc0ad835af4
Diana Damian <diana.damiaan@gmail.com>
2022-12-16 14:12:57 +0200

    Refactor the event listeners for numbers to use data attributes (#11)

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let btn6 = document.querySelector('#btn-6');
 let btn7 = document.querySelector('#btn-7');
 let btn8 = document.querySelector('#btn-8');
 let btn9 = document.querySelector('#btn-9');
+let btn0 = document.querySelector('#btn-0');
 let btnClear = document.querySelector('#clearBtn');
 let btnSubtraction = document.querySelector('#btn-subtraction');
 let btnAddition = document.querySelector('#btn-addition');
@@ -19,114 +20,31 @@ let operation = undefined;
 let nr1 = 0;
 let nr2 = 0;
 
-btn1.addEventListener('click', function () {
-    if (operation === undefined) {
-        nr1 = nr1 * 10 + 1;
-        calcScreen.innerHTML = nr1;
-        console.log('nr1', nr1);
-    } else {
-        nr2 = nr2 * 10 + 1;
-        calcScreen.innerHTML = nr2;
-        console.log('nr2', nr2);
-    }
-});
-
-btn2.addEventListener('click', function () {
-    if (operation === undefined) {
-        nr1 = nr1 * 10 + 2;
-        calcScreen.innerHTML = nr1;
-        console.log('nr1', nr1);
-    } else {
-        nr2 = nr2 * 10 + 2;
-        calcScreen.innerHTML = nr2;
-        console.log('nr2', nr2);
-    }
-});
-
-btn3.addEventListener('click', function () {
-    if (operation === undefined) {
-        nr1 = nr1 * 10 + 3;
-        calcScreen.innerHTML = nr1;
-        console.log('nr1', nr1);
-    } else {
-        nr2 = nr2 * 10 + 3;
-        calcScreen.innerHTML = nr2;
-        console.log('nr2', nr2);
-    }
-});
-
-btn4.addEventListener('click', function () {
-    if (operation === undefined) {
-        nr1 = nr1 * 10 + 4;
-        calcScreen.innerHTML = nr1;
-        console.log('nr1', nr1);
-    } else {
-        nr2 = nr2 * 10 + 4;
-        calcScreen.innerHTML = nr2;
-        console.log('nr2', nr2);
-    }
-});
-
-btn5.addEventListener('click', function () {
-    if (operation === undefined) {
-        nr1 = nr1 * 10 + 5;
-        calcScreen.innerHTML = nr1;
-        console.log('nr1', nr1);
-    } else {
-        nr2 = nr2 * 10 + 5;
-        calcScreen.innerHTML = nr2;
-        console.log('nr2', nr2);
-    }
-});
-
-btn6.addEventListener('click', function () {
-    if (operation === undefined) {
-        nr1 = nr1 * 10 + 6;
-        calcScreen.innerHTML = nr1;
-        console.log('nr1', nr1);
-    } else {
-        nr2 = nr2 * 10 + 6;
-        calcScreen.innerHTML = nr2;
-        console.log('nr2', nr2);
-    }
-});
-
-btn7.addEventListener('click', function () {
-    if (operation === undefined) {
-        nr1 = nr1 * 10 + 7;
-        calcScreen.innerHTML = nr1;
-        console.log('nr1', nr1);
-    } else {
-        nr2 = nr2 * 10 + 7;
-        calcScreen.innerHTML = nr2;
-        console.log('nr2', nr2);
-    }
-});
+btn1.addEventListener('click', onDigitClick);
+btn2.addEventListener('click', onDigitClick);
+btn3.addEventListener('click', onDigitClick);
+btn4.addEventListener('click', onDigitClick);
+btn5.addEventListener('click', onDigitClick);
+btn6.addEventListener('click', onDigitClick);
+btn7.addEventListener('click', onDigitClick);
+btn8.addEventListener('click', onDigitClick);
+btn9.addEventListener('click', onDigitClick);
+btn0.addEventListener('click', onDigitClick);
 
 
-btn8.addEventListener('click', function () {
+function onDigitClick(event) {
+    let digit = +event.currentTarget.getAttribute("data-number");
     if (operation === undefined) {
-        nr1 = nr1 * 10 + 8;
+        nr1 = nr1 * 10 + digit;
         calcScreen.innerHTML = nr1;
-        console.log('nr1', nr1);
+        console.log(nr1);
     } else {
-        nr2 = nr2 * 10 + 8;
+        nr2 = nr2 * 10 + digit;
         calcScreen.innerHTML = nr2;
-        console.log('nr2', nr2);
-    }
-});
+        console.log(nr2);
 
-btn9.addEventListener('click', function () {
-    if (operation === undefined) {
-        nr1 = nr1 * 10 + 9;
-        calcScreen.innerHTML = nr1;
-        console.log('nr1', nr1);
-    } else {
-        nr2 = nr2 * 10 + 9;
-        calcScreen.innerHTML = nr2;
-        console.log('nr2', nr2);
     }
-});
+}
 
 btnClear.addEventListener('click', function () {
     nr1 = 0;
